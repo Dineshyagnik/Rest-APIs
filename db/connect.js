a18259c75afb000912a1ecddcfded1dcd9506aae
Dinesh Kumar <101325099+Dineshyagnik@users.noreply.github.com>
2024-11-21 17:08:25 +0530

    secure data with dotenv

--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
-uri = "mongodb+srv://dinesh:mD3IfnhTCnsWxSOA@cluster0.exrt6.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0"
-
-const connectDB = ()=>{
+const connectDB = (uri) => {
     // console.log('connect DB');
-    
-    return mongoose.connect(uri,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
+
+    return mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     });
 };
 
