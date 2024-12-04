1b4dd62d351b43db258425a04759b1ab827f763b
Dinesh Kumar <101325099+Dineshyagnik@users.noreply.github.com>
2024-12-04 15:57:55 +0530

    create rest api

--- index.js
+++ index.js
@@ -1,57 +1,128 @@
-const http = require("http");
-// const fs = require("fs");
-// const url = require('url');
-const express = require('express');
+// const http = require("http");
+// // const fs = require("fs");
+// // const url = require('url');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.get('/', (req, res) => {
-    return res.end('Hello from Home page');
-});
-
-app.get('/about', (req, res) => {
-    return res.end('Hello from About page' + ' Hey ' + req.query.name + ' your age is ' + req.query.age);
-});
-
-
-
-
-// function myHandler(req, res) {
-//     if (req.url === '/favicon.ico') return res.end();
-//     const log = `${Date.now()}: ${req.method} ${req.url} New Request Received\n`;
-//     const myUrl = url.parse(req.url, true);
-//     console.log(myUrl);
-
-//     fs.appendFile("log.txt", log, (err, data) => {
-//         switch (myUrl.pathname) {
-//             case '/':
-//                 if (req.method === 'GET') res.end('Home Page');
-//                 break;
-//             case '/about':
-//                 const username = myUrl.query.myname
-//                 res.end(`Hi ${username}`);
-//                 break;
-//             case '/search':
-//                 const search = myUrl.query.search_query;
-//                 res.end('Here are your results for ' + search);
-//                 break;
-//             case '/contact': res.end('Contact Page')
-//                 break;
-//             case '/signup':
-//                 if (req.method === 'GET') res.end('This is a signup form');
-//                 else if (req.method === 'POST') {
-//                     //DB Query
-//                     res.end('Signup Successfull');
-//                 }
-//             default: res.end('404 not found')
-//         }
-//     });
-// }
-
-// const myServer = http.createServer(app);
-
-// myServer.listen(8000, () => {
-//     console.log("server started");
+// app.get('/', (req, res) => {
+//     return res.end('Hello from Home page');
 // });
 
-app.listen(8000, () => console.log('server started'));
+// app.get('/about', (req, res) => {
+//     return res.end('Hello from About page' + ' Hey ' + req.query.name + ' your age is ' + req.query.age);
+// });
+
+
+
+
+// // function myHandler(req, res) {
+// //     if (req.url === '/favicon.ico') return res.end();
+// //     const log = `${Date.now()}: ${req.method} ${req.url} New Request Received\n`;
+// //     const myUrl = url.parse(req.url, true);
+// //     console.log(myUrl);
+
+// //     fs.appendFile("log.txt", log, (err, data) => {
+// //         switch (myUrl.pathname) {
+// //             case '/':
+// //                 if (req.method === 'GET') res.end('Home Page');
+// //                 break;
+// //             case '/about':
+// //                 const username = myUrl.query.myname
+// //                 res.end(`Hi ${username}`);
+// //                 break;
+// //             case '/search':
+// //                 const search = myUrl.query.search_query;
+// //                 res.end('Here are your results for ' + search);
+// //                 break;
+// //             case '/contact': res.end('Contact Page')
+// //                 break;
+// //             case '/signup':
+// //                 if (req.method === 'GET') res.end('This is a signup form');
+// //                 else if (req.method === 'POST') {
+// //                     //DB Query
+// //                     res.end('Signup Successfull');
+// //                 }
+// //             default: res.end('404 not found')
+// //         }
+// //     });
+// // }
+
+// // const myServer = http.createServer(app);
+
+// // myServer.listen(8000, () => {
+// //     console.log("server started");
+// // });
+
+// app.listen(8000, () => console.log('server started'));
+
+
+
+
+const express = require('express');
+const users = require('./MOCK_DATA.json');
+const app = express();
+
+const PORT = 8000;
+
+//routes
+
+app.get('/users', (req, res) => {
+    const html = `
+        <ul>
+            ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `;
+    res.send(html)
+});
+
+// Rest API
+app.get('/api/users', (req, res) => {
+    return res.json(users);
+});
+
+app.route('/api/users/:id').get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+}).patch((req, res) => {
+    // Edit user with id
+    return res.json({ status: "pending" })
+})
+    .delete((req, res) => {
+        //Delete user with id
+        return res.json({ status: "pending" })
+    });
+
+
+app.post('/api/users', (req, res) => {
+    // TO Do Create new user
+    return res.json({ status: 'pending' });
+});
+
+
+// app.get('/api/users/:id', (req, res) => {
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+// });
+
+// app.post('/api/users',(req,res)=>{
+//     // TO Do Create new user
+//     return res.json({status:'pending'});
+// });
+
+// app.patch('/api/users/:id',(req,res)=>{
+//     // TO Do Edit the user with id
+//     return res.json({status:'pending'});
+// });
+
+// app.delete('/api/users/:id',(req,res)=>{
+//     // TO Do Delete the user with id
+//     return res.json({status:'pending'});
+// });
+
+
+
+
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
