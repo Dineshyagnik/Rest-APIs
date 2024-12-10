1706db22768c47c28b9301230cffd6df3792dd30
Dinesh Kumar <101325099+Dineshyagnik@users.noreply.github.com>
2024-12-10 16:18:04 +0530

    mvc pattern

--- index.js
+++ index.js
@@ -1,109 +1,26 @@
 const express = require('express');
-const fs = require('fs');
-const mongoose = require('mongoose');
-const app = express();
+const { connectMongoDb } = require('./connection');
 
+const { logReqRes } = require('./middlewares');
+
+const userRouter = require('./routes/user');
+
+const app = express();
 const PORT = 8000;
 
 // Connection
-mongoose.connect('mongodb://127.0.0.1:27017/youtube-app-1')
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log('Mongo Error', err));
+connectMongoDb("mongodb://127.0.0.1:27017/youtube-app-1").then(()=>
+    console.log("MongoDB Connected!")
+);
 
-// Schema
-const userSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    jobTitle: {
-        type: String,
-    },
-    gender: {
-        type: String,
-    }
-}, { timestamps: true });
-
-const User = mongoose.model("user", userSchema);
 
 //Middleware - Plugin
 app.use(express.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-    // fs.appendFile('log.txt', `${Date.now()}: ${req.ip}: ${req.method}: ${req.path}\n`, (err, data) => {
-    //     next();
-    // });
-});
-
-app.use((req, res, next) => {
-    next();
-});
-
-//routes
-
-app.get('/users', async (req, res) => {
-    const allDbUsers = await User.find({});
-    const html = `
-        <ul>
-            ${allDbUsers.map(user => `<li>${user.firstName} - ${user.email}</li>`).join("")}
-        </ul>
-    `;
-    res.send(html)
-});
-
-// Rest API
-app.get('/api/users', async (req, res) => {
-    const allDbUsers = await User.find({});
-    return res.json(allDbUsers);
-});
-
-app.route('/api/users/:id')
-    .get(async (req, res) => {
-        const user = await User.findById(req.params.id)
-
-        if (!user) return res.status(404).json({ err: "User not found" })
-        return res.json(user);
-    })
-    .patch(async (req, res) => {
-        await User.findByIdAndUpdate(req.params.id, { lastName: 'Changed' })
-        return res.json({ status: "success" })
-    })
-    .delete(async (req, res) => {
-        await User.findByIdAndDelete(req.params.id)
-        return res.json({ status: "success" })
-    });
+app.use(logReqRes('log.txt'));
 
 
-app.post('/api/users', async (req, res) => {
-    const body = req.body;
-    if (!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title) {
-        return res.status(400).json({ msg: "Please fill all fields" });
-    }
-    // users.push({ ...body, id: users.length + 1 });
-    // fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-    //     return res.status(201).json({ status: 'success', id: users.length });
-    // });
-
-    const result = await User.create({
-        firstName: body.first_name,
-        lastName: body.last_name,
-        email: body.email,
-        gender: body.gender,
-        jobTitle: body.job_title,
-    });
-    console.log("result: ", result);
-
-    return res.status(201).json({ msg: "success" })
-});
-
+// Routes 
+app.use("/api/users", userRouter);
 
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
---
+++ routes/user.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const {
+    handleGetAllUsers,
+    handlegetUserById,
+    handleUpdateUserById,
+    handleDeleteUserById,
+    handleCreateNewUser
+} = require("../controllers/user");
+
+const router = express.Router();
+
+// Rest API
+router.route("/")
+    .get(handleGetAllUsers)
+    .post(handleCreateNewUser);
+
+router
+    .route("/:id")
+    .get(handlegetUserById)
+    .patch(handleUpdateUserById)
+    .delete(handleDeleteUserById);
+
+module.exports = router;
